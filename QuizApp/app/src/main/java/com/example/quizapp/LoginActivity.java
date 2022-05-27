10122b2ae7508ab8b9d17757d1cfe6e8a4e67aba
mohamed-ait <mohamedaitlahcen62@gmail.com>
2022-05-27 16:59:58 +0100

    create LoginActivity class

--- QuizApp/app/src/main/java/com/example/quizapp/LoginActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     EditText username, password;
